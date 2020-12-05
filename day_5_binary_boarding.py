cad0ef32413991cb711a536b2ac2025575bb1c6a
MyerFire <myercheng0614@gmail.com>
2020-12-05 01:45:50 -0500

    day 4

---
+++ day_5_binary_boarding.py
@@ -0,0 +1,44 @@
+class BoardingPass:
+    def __init__(self, data):
+        self.row, self.column, self.id = parse(data)  # returns tuple
+
+
+def parse(data):
+    row_specification = data[:7]
+    column_specification = data[-3:]
+    row = list(range(0, 128))
+    column = list(range(0, 8))
+    for character in row_specification:
+        if character == "F":
+            row = row[:len(row) // 2]
+        if character == "B":
+            row = row[len(row) // 2:]
+    for character in column_specification:
+        if character == "L":
+            column = column[:len(column) // 2]
+        elif character == "R":
+            column = column[len(column) // 2:]
+    id_ = row[0] * 8 + column[0]
+    return row[0], column[0], id_
+
+
+def sort_key(boarding_pass):
+    return boarding_pass.id
+
+
+with open("Inputs/day_5.txt") as input_:
+    boarding_passes = input_.readlines()
+    boarding_passes = [boarding_pass.replace("\n", "") for boarding_pass in boarding_passes]
+
+if __name__ == "__main__":
+    boarding_passes = [BoardingPass(boarding_pass) for boarding_pass in boarding_passes]
+    boarding_passes.sort(key=sort_key, reverse=True)
+    print(f"{boarding_passes[0].id} is the highest seat ID")
+    print("-" * 10)
+    for index, boarding_pass in enumerate(boarding_passes):
+        if index == 0:
+            pass
+        else:
+            if boarding_pass.id + 1 != boarding_passes[index - 1].id:
+                print(f"There is a missing seat between seat ID {boarding_pass.id} and {boarding_passes[index - 1].id}"
+                      f", which is {(boarding_pass.id + boarding_passes[index - 1].id) // 2}")
